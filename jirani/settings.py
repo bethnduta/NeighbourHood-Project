be5affaafcd2656465a2f2bf7817d366fcdc778b
beth nduta <bethnduta05@gmail.com>
2022-06-20 07:15:36 +0300

    login and register

--- jirani/settings.py
+++ jirani/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'neighbor',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
--- neighbor/views.py
+++ neighbor/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .forms import UserRegisterForm
 # Create your views here.
 posts=[
     {
-        'author':'CoreyMS',
+        'author':'bethy',
         'title':'Blog Post 1',
         'content':'First post content',
         'date_posted':'August 27, 2018'
@@ -22,3 +23,15 @@ def index(request):
 
 def neighbor(request):
     return render(request, 'about.html',{'title':'neighbor'})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            
+            form.save()
+        return redirect('login')
+        
+    else:    
+        form = UserRegisterForm()
+    return render(request,'register.html',{'form':form})
