ff4c5da3c1c09fa9a80caf05fd375c431a6a5d22
beth nduta <bethnduta05@gmail.com>
2022-06-21 15:46:40 +0300

    tests

--- neighbor/tests.py
+++ neighbor/tests.py
@@ -8,6 +8,22 @@ class Test(TestCase):
 class post_test(TestCase):
     def test_is_true(self):
         self.assertTrue(True)
+
+class business_test(TestCase):
+    def test_is_true(self):
+        self.assertTrue(True)
+
+class profile_test(TestCase):
+    def test_is_true(self):
+        self.assertTrue(True)
+
+class neighborhood_test(TestCase):
+    def test_is_true(self):
+        self.assertTrue(True)
+
+class user_test(TestCase):
+    def test_is_true(self):
+        self.assertTrue(True)         
    
 
 
