c7bab893222946858c87801c9718f6705c93cf98
beth nduta <bethnduta05@gmail.com>
2022-06-21 16:34:56 +0300

    tested the methods

--- neighbor/tests.py
+++ neighbor/tests.py
@@ -1,39 +1,59 @@
 from django.test import TestCase
-
+from .models import post, profile, NeighborHood, Business
 # Create your tests here.
-def test_create_neighborhood():
-    assert True
+               
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.business = Business(name='fish palor', location='Rpysambu', occupants='0', description='description', admin='admin', police_number=999, health_number=07123456)
+        self.business.save()
 
-def test_update_neighborhood():
-    assert True
+    def test_instance(self):
+        self.assertTrue(isinstance(self.business, Business))
 
-def test_update_occupants():
-    assert True
+    def tearDown(self):
+        Business.objects.all().delete()
 
-def test_delete_neighborhood():
-    assert True
+    def test_save_business(self):
+        self.business.save_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business) > 0)
 
-def test_create_post():
-    assert True
+    def test_delete_business(self):
+        self.business.delete_business()
+        business = Business.objects.all()
+        self.assertTrue(len(business) == 0)
 
-def test_update_post():
-    assert True
+    def test_search_business(self):
+        self.business.save_business()
+        business = Business.search_business('fish')
+        self.assertTrue(len(business) > 0)   
 
-def test_delete_post():
-    assert True                
-
-
-class Test(TestCase):
+class ProfileTestClass(TestCase):
     def test_is_true(self):
         self.assertTrue(True)
+
+    def tearDown(self) :
+        profile.objects.all().delete()
+        post.objects.all().delete() 
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, post))     
+
 
 class post_test(TestCase):
-    def test_is_true(self):
-        self.assertTrue(True)
+    def setup(self):
+        self.neighborhood = NeighborHood(name='fish palor', location='Rpysambu', occupants='0', description='description', admin='admin', police_number=999, health_number=07123456)
+        self.neighborhood.save()
 
-class business_test(TestCase):
-    def test_is_true(self):
-        self.assertTrue(True)
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighborhood, NeighborHood))
+
+    def test_delete_method(self):
+        self.neighborhood.delete_neighborhood()
+        neighborhoods = NeighborHood.objects.all()
+        self.assertTrue(len(neighborhoods) == 0)       
+
+         
 
 class profile_test(TestCase):
     def test_is_true(self):
