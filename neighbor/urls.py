3833e42ffe4ce0dafad24505343570d0bd4acc37
beth nduta <bethnduta05@gmail.com>
2022-06-23 16:04:38 +0300

    modified views,urls&templates

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -5,11 +5,12 @@ from .views import PostListView,PostDetailView
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='index'),
+    path('',views.home,name='home'),
+    path('index/', PostListView.as_view(), name='index'),
     path('about/', views.neighbor, name='about'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', views.post_create, name='post-create'),
-    # path('neighborhood',views.neighborhood,name='neighborhood'),
+    path('neighborhood',views.neighborhood,name='neighborhood'),
     # path('new_neighborhood',views.new_neighborhood,name='new_neighborhood'),
     # path('single_neighborhood/<int:pk>/',views.single_neighborhood,name='single_neighborhood'),
     # path('new_neighborhood/<int:pk>/',views.new_neighborhood,name='new_neighborhood'),
--- neighbor/views.py
+++ neighbor/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from .models import Business, post
 from .forms import UserRegisterForm
 from django.contrib import messages
-from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm
+from .forms import UserRegisterForm,UserUpdateForm,ProfileUpdateForm,neighborhoodForm
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView, DetailView, CreateView,  UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
@@ -12,6 +12,10 @@ from .forms import PostForm
 
 # Create your views here.
 
+def home(request):
+    return render(request,'home.html')
+
+@login_required
 def index(request):
     context = {'posts':post.objects.all()}
     return render(request, 'index.html', context)
@@ -40,7 +44,7 @@ def profile(request):
             u_form.save()
             p_form.save()
             messages.success(request,f'Account updated successfully')
-            return redirect('profile')
+            return redirect('index')
     u_form=UserUpdateForm(instance=request.user)
     p_form=ProfileUpdateForm(instance=request.user)
 
@@ -102,27 +106,27 @@ class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
 #         form = BusinessForm()
 #     return render(request,'business_form.html',{'form':form})
 
-# @login_required
-# def neighborhood(request):
-#     if request.method == 'POST':
-#         form = NeighborhoodForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('index')
-#     else:
-#         form = NeighborhoodForm()
-#     return render(request,'neighborhood_form.html',{'form':form})
+@login_required
+def neighborhood(request):
+    if request.method == 'POST':
+        form = neighborhoodForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = neighborhoodForm()
+    return render(request,'neighborhood_form.html',{'form':form})
 
 
-# def create_neighborhood(request):
-#     if request.method == 'POST':
-#         form = NeighborhoodForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('index')
-#     else:
-#         form = NeighborhoodForm()
-#     return render(request,'neighborhood_form.html',{'form':form})
+def create_neighborhood(request):
+    if request.method == 'POST':
+        form = neighborhoodForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = neighborhoodForm()
+    return render(request,'neighborhood_form.html',{'form':form})
 
 # @login_required
 # def single_neighborhood(request,neighborhood_id):
@@ -138,19 +142,19 @@ class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
 #             form = BusinessForm()    
 #     return render(request,'single_neighborhood.html',{'neighborhood':neighborhood})
 
-# @login_required
-# def join_neighborhood(request,neighborhood_id):
-#     neighborhood=Neighborhood.objects.get(id=neighborhood_id)
-#     request.user.profile.neighborhood.add(neighborhood)
-#     request.user.profile.save()  
-#     return render(request,'single_neighborhood.html',{'neighborhood':neighborhood})            
+@login_required
+def join_neighborhood(request,neighborhood_id):
+    neighborhood=neighborhood.objects.get(id=neighborhood_id)
+    request.user.profile.neighborhood.add(neighborhood)
+    request.user.profile.save()  
+    return render(request,'single_neighborhood.html',{'neighborhood':neighborhood})            
 
-# @login_required
-# def leave_neighborhood(request,neighborhood_id):
-#     neighborhood=Neighborhood.objects.get(id=neighborhood_id)
-#     request.user.profile.neighborhood.remove(neighborhood)
-#     request.user.profile.save()  
-#     return render(request,'single_neighborhood.html',{'neighborhood':neighborhood})
+@login_required
+def leave_neighborhood(request,neighborhood_id):
+    neighborhood=neighborhood.objects.get(id=neighborhood_id)
+    request.user.profile.neighborhood.remove(neighborhood)
+    request.user.profile.save()  
+    return render(request,'single_neighborhood.html',{'neighborhood':neighborhood})
 
 # def search_business(request):
 #     if request.method == 'POST':
